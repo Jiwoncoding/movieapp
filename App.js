c7b8fa9d25a58d00f9681bcbd67e2581842d8a06
JiwontwoPunch <80305292+Jiwoncoding@users.noreply.github.com>
2021-12-04 18:46:16 +0900

    Add files via upload
    주석으로 이론 추가

--- App.js
+++ App.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-function App() {
+// 짧은 코드로 작성할 수 있는 컴포넌트는 모두 App안에 작성한다. → import X, function로 시작
 
+function App() {
+    return(
+        <div>
+         <h1>hi</h1>
+        </div>
+    );
 }
 
 export default App;
--- index.js
+++ index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+
+// ReactDOM.render() 함수의 첫 번째 인자는 컴포넌트 두 번째 인자는 컴포넌트가 그려질 위치
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+    <App />, document.getElementById('root')
 );
