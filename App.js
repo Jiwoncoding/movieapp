3d98628399ca42801bed53321698a54651939074
JiwontwoPunch <80305292+Jiwoncoding@users.noreply.github.com>
2021-12-18 22:51:04 +0900

    Add files via upload
    prop-types 도구 npm을 통해 설치, 주석으로 설명하기에 긴 이론들 정리를 위해 explanation,txt 파일 생성

--- App.js
+++ App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 // 짧은 코드로 작성할 수 있는 컴포넌트는 모두 App안에 작성한다. → import X, function로 시작
+function Food({fav}){
+    return <h1>I like {fav}</h1>;
+}
+
+// 서버에서 넘어온 데이터를 저장할 수 있도록 변수를 만든 다음 빈 배열을 할당
+const foodILike = [];
 
 function App() {
     return(
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 
 
-// ReactDOM.render() 함수의 첫 번째 인자는 컴포넌트 두 번째 인자는 컴포넌트가 그려질 위치
+// ReactDOM.render() 함수의 첫 번째 인자는 컴포넌트, 두 번째 인자는 컴포넌트가 그려질 위치
 ReactDOM.render(
     <App />, document.getElementById('root')
 );
