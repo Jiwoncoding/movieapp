0d7e71e9d6e87484644b1efb5d838c07c22f17ef
JiwontwoPunch <80305292+Jiwoncoding@users.noreply.github.com>
2021-12-22 15:33:34 +0900

    Add files via upload
    영화앱만들기 워밍업 설정

--- App.js
+++ App.js
@@ -1,31 +1,22 @@
 import React from 'react';
 
-// 짧은 코드로 작성할 수 있는 컴포넌트는 모두 App안에 작성한다. → import X, function로 시작
-// function Food({fav}){
-//     return <h1>I like {fav}</h1>;
-// }
-
-// 서버에서 넘어온 데이터를 저장할 수 있도록 변수를 만든 다음 빈 배열을 할당
-// const foodILike = [];
-
 class App extends React.Component {
-    // state 정의하기
     state = {
-        count : 0,
+        isLoading: true,  // 로딩 중 표시기능
+        movies: [],  // 로딩된 영화 데이터를 저장
     };
 
-    // 숫자 증감 기능 만들기
-    add = () => this.setState(current => ({count:current.count + 1}));
-
-    minus = () => this.setState(current => ({count:current.count - 1}));
+    // 로딩 현상 구현
+    componentDidMount(){
+        // 영화 데이터 로딩!
+        setTimeout(()=>{
+            this.setState({isLoading: false});  // state를 바꾸려면 setState()
+        }, 6000);
+    }
 
     render(){
-        return(
-        <div><h1>The number is : {this.state.count}</h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-        </div>
-        );
+        const {isLoading} = this.state;
+        return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
     }
 }
 
