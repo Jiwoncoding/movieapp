ec21890eb4c30d78a6b10c9ffa2d2387358fcbdd
JiwontwoPunch <80305292+Jiwoncoding@users.noreply.github.com>
2021-12-29 00:59:25 +0900

    Add files via upload
    영화 API 사용해보기, axios.get()을 실행하려면 시간이 필요하고, 그 사실을 자바스크립트에게 알리기 위해 async, await를 사용

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React from 'react';
 
 class App extends React.Component {
@@ -5,13 +6,21 @@ class App extends React.Component {
         isLoading: true,  // 로딩 중 표시기능
         movies: [],  // 로딩된 영화 데이터를 저장
     };
+    getMovies = async () => {  // 자바스크립트에게 getMovies() 함수는 시간이 필요하고 
+        const movies = await axios.get("https://yts-proxy.now.sh/list_moviex.json");
+                       // axios.get()의 실행을 기다려 달라고 말해주는 것
+    }
 
     // 로딩 현상 구현
     componentDidMount(){
         // 영화 데이터 로딩!
-        setTimeout(()=>{
-            this.setState({isLoading: false});  // state를 바꾸려면 setState()
-        }, 6000);
+        // setTimeout(()=>{
+        //     this.setState({isLoading: false});  // state를 바꾸려면 setState()
+        // }, 6000);
+
+        // axios로 API 호출
+        // axios.get('https://yts-proxy.now.sh/list_movies.json');
+        this.getMovies();
     }
 
     render(){
