893477f6d27b6339aa65d3c20d5ba17928b99c05
JiwontwoPunch <80305292+Jiwoncoding@users.noreply.github.com>
2022-01-02 21:03:43 +0900

    Add files via upload
    영화 앱 스타일링

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import React from 'react';
 import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component {
     state = {
@@ -32,11 +33,15 @@ class App extends React.Component {
 
     render(){
         const {isLoading, movies} = this.state;
-        return ( <div>
-         {isLoading
-          ? 'Loading...' 
-          : movies.map((movie)=>{
-            return (
+        return ( 
+        <section className="container">
+         {isLoading ? (
+             <div className="loader">
+                <span className="loader__text">Loading...</span>
+             </div>
+         ) : (
+         <div className="movies">
+          { movies.map((movie)=> (
                 <Movie
                     key={movie.id}
                     id={movie.id}
@@ -44,10 +49,12 @@ class App extends React.Component {
                     title={movie.title}
                     summary={movie.summary}
                     poster={movie.medium_cover_image}
+                    genres={movie.genres}
                 />
-            );
-        })}
-        </div>
+            ))}
+            </div>
+        )}
+        </section>
         );
     }
 }
--- Movie.js
+++ Movie.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Movie.css';
 
-function Movie({ id, year, title, summary, poster }) {
-    return (<h4>{ title }</h4>);
+function Movie({ year, title, summary, poster, genres }) {
+    return (
+        <div className="movie">
+            <img src={poster} alt={title} title={title} />
+        <div className="movie__date">
+            <h3 className="movie__title">{title}</h3>
+            <h5 className="movie__year">{year}</h5>
+            <ul className='movie__genres'>
+                {genres.map((genre, index)=>{
+                    return <li key={index} className='movie__genre'>{genre}</li>
+                })}
+            </ul>
+            <p className="movie__summary">{summary.slice(0,180)}...</p>
+        </div>
+     </div>
+    );
 }
 
-Movie.propTypes = {
-    id: PropTypes.number.isRequired,   // API에서 id는 자료형이 Number, 반드시 있어야하니까 isRequired
-    year: PropTypes.number.isRequired,
+Movie.propTypes = {  
+    year: PropTypes.number.isRequired,   // API에서 year는 자료형이 Number, 반드시 있어야하니까 isRequired
     title: PropTypes.string.isRequired,
     summary: PropTypes.string.isRequired,
     poster: PropTypes.string.isRequired,  // poster에는 이미지 주소(문자열)가 저장
+    genres: PropTypes.arrayOf(PropTypes.string).isRequired,
 }; 
 
 export default Movie;
