fc217d604df9cfa609611aab0f7f6c980f801c3b
JiwontwoPunch <80305292+Jiwoncoding@users.noreply.github.com>
2022-01-03 14:55:58 +0900

    Add files via upload
    React를 사용한 Movieapp clone하기 완성

--- App.js
+++ App.js
@@ -1,62 +1,22 @@
 import axios from 'axios';
 import React from 'react';
-import Movie from './Movie';
 import './App.css';
+import { HashRouter, Route} from 'react-router-dom';
+import About from './routes/About';
+import Home from './routes/Home';
+import Navigation from './components/Navigation';
+import Detail from './routes/Detail';
 
-class App extends React.Component {
-    state = {
-        isLoading: true,  // 로딩 중 표시기능
-        movies: [],  // 로딩된 영화 데이터를 저장
-    };
-    getMovies = async () => {  // 자바스크립트에게 getMovies() 함수는 시간이 필요하고 
-        const {
-            data: {
-                data: { movies },   // 점 연산자 순서대로 구조 분해 할당 적용
-            },
-        } = await axios.get("https://yts-proxy.now.sh/list_moviex.json?sort_by=rating");
-                       // axios.get()의 실행을 기다려 달라고 말해주는 것
-        this.setState({ movies, isLoading: false }); // ES6에서는 객체의 키와 대입할 변수의 이름이 같다면 코드를 축약할 수 있다.
-
-    };
-
-    // 로딩 현상 구현
-    componentDidMount(){
-        // 영화 데이터 로딩!
-        // setTimeout(()=>{
-        //     this.setState({isLoading: false});  // state를 바꾸려면 setState()
-        // }, 6000);
-
-        // axios로 API 호출
-        // axios.get('https://yts-proxy.now.sh/list_movies.json');
-        this.getMovies();
-    }
-
-    render(){
-        const {isLoading, movies} = this.state;
-        return ( 
-        <section className="container">
-         {isLoading ? (
-             <div className="loader">
-                <span className="loader__text">Loading...</span>
-             </div>
-         ) : (
-         <div className="movies">
-          { movies.map((movie)=> (
-                <Movie
-                    key={movie.id}
-                    id={movie.id}
-                    year={movie.year}
-                    title={movie.title}
-                    summary={movie.summary}
-                    poster={movie.medium_cover_image}
-                    genres={movie.genres}
-                />
-            ))}
-            </div>
-        )}
-        </section>
-        );
-    }
+function App(){
+    return(
+        <HashRouter>
+            <Navigation />
+            <Route path="/" exact={true} component={Home} /> 
+            <Route path="/about" component={About} />
+            <Route path="/movie-detail" component={Detail} />
+        </HashRouter>
+    )
 }
+
 
 export default App;
---
+++ Home.js
@@ -0,0 +1,62 @@
+import axios from 'axios';
+import React from 'react';
+import Movie from '../components/Movie';
+import './Home.css';
+
+class Home extends React.Component {
+    state = {
+        isLoading: true,  // 로딩 중 표시기능
+        movies: [],  // 로딩된 영화 데이터를 저장
+    };
+    getMovies = async () => {  // 자바스크립트에게 getMovies() 함수는 시간이 필요하고 
+        const {
+            data: {
+                data: { movies },   // 점 연산자 순서대로 구조 분해 할당 적용
+            },
+        } = await axios.get("https://yts-proxy.now.sh/list_moviex.json?sort_by=rating");
+                       // axios.get()의 실행을 기다려 달라고 말해주는 것
+        this.setState({ movies, isLoading: false }); // ES6에서는 객체의 키와 대입할 변수의 이름이 같다면 코드를 축약할 수 있다.
+
+    };
+
+    // 로딩 현상 구현
+    componentDidMount(){
+        // 영화 데이터 로딩!
+        // setTimeout(()=>{
+        //     this.setState({isLoading: false});  // state를 바꾸려면 setState()
+        // }, 6000);
+
+        // axios로 API 호출
+        // axios.get('https://yts-proxy.now.sh/list_movies.json');
+        this.getMovies();
+    }
+
+    render(){
+        const {isLoading, movies} = this.state;
+        return ( 
+        <section className="container">
+         {isLoading ? (
+             <div className="loader">
+                <span className="loader__text">Loading...</span>
+             </div>
+         ) : (
+         <div className="movies">
+          { movies.map((movie)=> (
+                <Movie
+                    key={movie.id}
+                    id={movie.id}
+                    year={movie.year}
+                    title={movie.title}
+                    summary={movie.summary}
+                    poster={movie.medium_cover_image}
+                    genres={movie.genres}
+                />
+            ))}
+            </div>
+        )}
+        </section>
+        );
+    }
+}
+
+export default Home;
